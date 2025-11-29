4ac167d402917663922b37c7e61864d2e4f021ed
Longzhou Xu <xlzhope@foxmail.com>
2025-11-29 11:18:13 +0800

    Update stepz_1_age_analysis.m

--- stepz_1_age_analysis.m
+++ stepz_1_age_analysis.m
@@ -15,21 +15,3 @@ ncount = connectivity;
 plot(qa(:), log(ncount(:)), '*')
 idx = find(ncount>0);
 [r,  p] = corr(qa(idx), log(ncount(idx)));
-load('whole_brain_Yeo2011_17Networks_MNI152NLin2009cAsym_1mm_LPS_qa.mat')
-qa = connectivity;
-load('whole_brain_Yeo2011_17Networks_MNI152NLin2009cAsym_1mm_LPS_ncount.mat')
-ncount = connectivity;
-figure;
-subplot(2, 2, 1)
-imagesc(log(ncount));
-title('ncount');
-subplot(2, 2, 2);
-imagesc(qa);
-title('QA');
-subplot(2, 2, 3);
-plot(log(ncount(:)), qa(:), '*')
-ylabel('QA')
-xlabel('ncount');
-title(['r=-0.1, p=0.08']);
-idx = find(ncount>0);
-[r,  p] = corr(qa(idx), log(ncount(idx)));
